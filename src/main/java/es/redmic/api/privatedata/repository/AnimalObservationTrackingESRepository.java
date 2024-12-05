47cc3de3db6b57bf60a055580c5d5676233ae363
Noel Alonso <naloexp@grafcan.com>
2024-12-05 11:14:27 +0000

    Añade etiqueta al repositorio

--- src/main/java/es/redmic/api/privatedata/repository/AnimalObservationTrackingESRepository.java
+++ src/main/java/es/redmic/api/privatedata/repository/AnimalObservationTrackingESRepository.java
@@ -4,14 +4,14 @@ package es.redmic.api.privatedata.repository;
  * #%L
  * API
  * %%
- * Copyright (C) 2019 - 2024 REDMIC Project / Server
+ * Copyright (C) 2024 - 2024 REDMIC Project / Server
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,8 +20,10 @@ package es.redmic.api.privatedata.repository;
  * #L%
  */
 
+import org.springframework.stereotype.Repository;
 import es.redmic.es.geodata.tracking.common.repository.TrackingBaseESRepository;
 
+@Repository
 public class AnimalObservationTrackingESRepository extends TrackingBaseESRepository {
 
 	protected static String[] INDEX = { "private-geodata" };
